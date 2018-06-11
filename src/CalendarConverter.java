2051f750bb5485e5a4d4f273f4f1a17f54579167
EwelinaGordon <ewelina.gordon@gmail.com>
2018-06-11 22:13:55 +0200

    Zadanie 7.1

---
+++ src/CalendarConverter.java
@@ -0,0 +1,38 @@
+public class CalendarConverter {
+    public static final int MONDAY = 1;
+    public static final int TUESDAY = 2;
+    public static final int WEDNESDAY = 3;
+    public static final int THURSDAY = 4;
+    public static final int FRIDAY = 5;
+    public static final int SATURDAY = 6;
+    public static final int SUNDAY = 7;
+
+
+    public static String convertDayToString(int dayNumber) {
+        String day = null;
+        switch (dayNumber) {
+            case MONDAY:
+                day = "Poniedziałek";
+                break;
+            case TUESDAY:
+                day = "Wtorek";
+                break;
+            case WEDNESDAY:
+                day = "Sroda";
+                break;
+            case THURSDAY:
+                day = "Czwartek";
+                break;
+            case FRIDAY:
+                day = "Piątek";
+                break;
+            case SATURDAY:
+                day = "Sobota";
+                break;
+            case SUNDAY:
+                day = "Niedziela";
+                break;
+        }
+        return day;
+    }
+}
